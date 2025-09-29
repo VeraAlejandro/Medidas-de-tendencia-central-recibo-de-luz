95bc3889374c33e038a14423081c9f6ed79fbc24
VeraAlejandro <git.vera10@gmail.com>
2025-09-29 01:01:29 -0600

    prueba dos actualizando codigo de barras

--- ProyectoLuz.R
+++ ProyectoLuz.R
@@ -27,6 +27,37 @@ print(paste("La Varianza para este dato de cuanto pagarons seria de: ", var(DfLu
 print(paste("La Varianza para este dato de cuanto pagarons seria de: ", diff(range((DfLuzR$LuzPesos)))))
 
 
+# Histograma de LuzPesos
+hist(DfLuzR$LuzPesos,
+     main = "Histograma de Luz en Pesos",
+     xlab = "Luz en Pesos",
+     ylab = "Frecuencia",
+     col = "lightblue",
+     border = "black")
+
+# Calcular el histograma sin graficar
+h <- hist(DfLuzR$LuzPesos, plot = FALSE)
+
+# Graficar polígono
+plot(h$mids, h$counts, type = "b", 
+     main = "Polígono de Frecuencias de LuzPesos",
+     xlab = "Luz en Pesos",
+     ylab = "Frecuencia",
+     col = "red",
+     pch = 19)
+# Grafica de barras 
+barplot(table(DfLuzR$Municipio),
+        main = "Número de Encuestados por Municipio",
+        xlab = "Municipio",
+        ylab = "Frecuencia",
+        col = rainbow(length(unique(DfLuzR$Municipio))))
+
+# Grafica pastel
+pie(table(DfLuzR$Municipio),
+    main = "Distribución de Encuestados por Municipio",
+    col = rainbow(length(unique(DfLuzR$Municipio))))
+
+#cuartiles
 cuartiles <- quantile(DfLuzR$LuzPesos)
 for (i in 2:4){
   mensaje <- paste("Cuartil", i-1 , names(cuartiles)[i], "es", cuartiles[i])
